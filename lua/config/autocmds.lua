a24d2ffc64dda8871b7c425edc6a35909a07be19
Memnoc <smartdroidesign@gmail.com>
2025-12-18 15:59:31 +0000

    init

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,18 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+-- Make C comments and ifdef colored
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "@lsp.type.comment.c", {})
+  end,
+})
+
+-- Make the buffer separation line more visible
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#e0af68" })
+  end,
+})
