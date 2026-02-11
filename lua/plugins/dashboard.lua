73a7b7ccb211bc8a93e65b5fb9aeb440aeca0850
Memnoc <smartdroidesign@gmail.com>
2026-02-11 11:24:27 +0000

    changed ASCII art

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -6,19 +6,19 @@ return {
     dashboard = {
       enabled = true,
       preset = {
-        header = [[
-███╗   ███╗███████╗███╗   ███╗███╗   ██╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗ ████║██╔════╝████╗ ████║████╗  ██║██╔═══██╗██║   ██║██║████╗ ████║
-██╔████╔██║█████╗  ██╔████╔██║██╔██╗ ██║██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██║╚██╗██║██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║██║ ╚████║╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+        header = table.concat({
+          [[  /\ \▔\___  ___/\   /(●)_ __ ___  ]],
+          [[ /  \/ / _ \/ _ \ \ / / | '_ ` _ \ ]],
+          [[/ /\  /  __/ (_) \ V /| | | | | | |]],
+          [[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
+          [[───────────────────────────────────]],
+        }, "\n"),
       },
       sections = {
         { section = "header" },
-        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { section = "startup" },
       },
     },
