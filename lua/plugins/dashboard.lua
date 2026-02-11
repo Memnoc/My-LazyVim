fab5f1aa48e629e1e95e56744c6164fd6ebd2f1a
Memnoc <smartdroidesign@gmail.com>
2026-02-11 11:25:46 +0000

    added old implementation too

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -24,3 +24,31 @@ return {
     },
   },
 }
+
+-- OLD
+-- return {
+--   "folke/snacks.nvim",
+--   priority = 1000,
+--   lazy = false,
+--   opts = {
+--     dashboard = {
+--       enabled = true,
+--       preset = {
+--       header = [[
+--        ███╗   ███╗███████╗███╗   ███╗███╗   ██╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+--        ████╗ ████║██╔════╝████╗ ████║████╗  ██║██╔═══██╗██║   ██║██║████╗ ████║
+--        ██╔████╔██║█████╗  ██╔████╔██║██╔██╗ ██║██║   ██║██║   ██║██║██╔████╔██║
+--        ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██║╚██╗██║██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+--        ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║██║ ╚████║╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+--        ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+--              },
+--       sections = {
+--         { section = "header" },
+--         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+--         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+--         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+--         { section = "startup" },
+--       },
+--     },
+--   },
+-- }
